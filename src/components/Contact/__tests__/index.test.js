b1ead993439e116b5d6359d82a3f4606d63090ab
superfishal <97133716+superfishal@users.noreply.github.com>
2022-06-14 13:27:00 -0700

    fixed testing issues

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -1,28 +1,29 @@
 // __tests__/Contact.test.js
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Contact from "../../Contact";
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Contact from '..'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
-describe("Contact component renders", () => {
-  it("renders", () => {
+describe('Contact component', () => {
+  it('renders', () => {
     render(<Contact />);
   });
 
-  it("renders", () => {
-    const { asFragment } = render(<Contact />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
+  it('renders', () => {
+    const { asFragment } = render(<Contact />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
 
-it("renders", () => {
-  const { getByTestId } = render(<Contact />);
-  expect(getByTestId("h1tag")).toHaveTextContent("Contact me");
-});
 
-it("renders", () => {
-  const { getByTestId } = render(<Contact />);
-  expect(getByTestId("button")).toHaveTextContent("Submit");
-});
+it('renders', () => {
+  const { getByTestId } = render(<Contact />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+})
+
+it('renders', () => {
+  const { getByTestId } = render(<Contact />)
+  expect(getByTestId('button')).toHaveTextContent('Submit')
+})
